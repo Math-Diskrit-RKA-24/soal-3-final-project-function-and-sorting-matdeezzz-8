9c1a5003852143f2d5e52d38f84818ac1407fbff
ilena031 <5054241003@student.its.ac.id>
2024-12-02 20:49:41 +0700

    Update game.py

--- src/game.py
+++ src/game.py
@@ -2,6 +2,13 @@ def initPlayers() :
 def createNewPlayer(name, damage=0, defensePower=0) :
 def addPlayer(player) : 
 def removePlayer(name) :
+  global PlayerList[]
+  for Player in PlayerList :
+    if Player['name'] = name :
+      return Playerlist.remove(player) 
+  return "There is no player with that name!"
+  
 def setPlayer(player, key, value) :
+  
 def attackPlayer(attacker:dict, target:dict) :
 def displayMatchResult() :
