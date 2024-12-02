3ad1829e2843bc7b113bc03b947397d2be83e32e
ilena031 <5054241003@student.its.ac.id>
2024-12-02 20:36:37 +0700

    Create game.py

---
+++ src/game.py
@@ -0,0 +1,7 @@
+def initPlayers() :
+def createNewPlayer(name, damage=0, defensePower=0) :
+def addPlayer(player) : 
+def removePlayer(name) :
+def setPlayer(player, key, value) :
+def attackPlayer(attacker:dict, target:dict) :
+def displayMatchResult() :
