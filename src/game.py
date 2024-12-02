3c4b72389b4de65b76e475fc380b628a7c0e7dc7
eycoo <ahmadnaufalfarras5@gmail.com>
2024-12-02 20:55:33 +0700

    Update game.py

--- src/game.py
+++ src/game.py
@@ -1,14 +1,33 @@
 def initPlayers() :
+  
+  global PlayerList
+  PlayerList = []
+  
 def createNewPlayer(name, damage=0, defensePower=0) :
-def addPlayer(player) : 
+  
+  return {name = name, 
+          score = 0, 
+          damage = damage, 
+          health =100,
+          defensePower = defensePower,
+          defense = false }
+ 
+def addPlayer(player) :
+  
+  global PlayerList
+  PlayerList.append(player)
+    
 def removePlayer(name) :
   global PlayerList[]
   for Player in PlayerList :
     if Player['name'] = name :
       return Playerlist.remove(player) 
   return "There is no player with that name!"
-  
+
 def setPlayer(player, key, value) :
   
+  player[key] = value
+
 def attackPlayer(attacker:dict, target:dict) :
+  
 def displayMatchResult() :
